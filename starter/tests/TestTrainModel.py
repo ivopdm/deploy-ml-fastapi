72480c249b36563fe2aa6f930b94936e54d1027a
Ivo Medeiros <ivopdm@gmail.com>
2023-02-24 16:13:00 -0300

    update github action and TestTrainModel

--- starter/tests/TestTrainModel.py
+++ starter/tests/TestTrainModel.py
@@ -1,10 +1,13 @@
-from model import compute_model_metrics, train_model
+import sys
+
+import pandas as pd
+sys.path.append('../ml')
+from model import compute_model_metrics, train_model, compute_model_metrics_by_slice
 import unittest
 
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-import sys
-sys.path.append('starter/ml')
+
 
 
 class TestTrainModel(unittest.TestCase):
@@ -32,20 +35,26 @@ class TestTrainModel(unittest.TestCase):
         self.assertEqual(precision, 0.5)
         self.assertEqual(recall, 0.5)
         self.assertEqual(fbeta, 0.5)
-
-    def test_compute_model_metrics_zero_division(self):
+    
+    # test compute_model_metrics_by_slice function
+    def test_compute_model_metrics_by_slice(self):
         # Generate some fake known labels and predicted labels
+        d = {'col1': ['A', 'A', 'B', 'B'], 'col2': [0, 0, 0, 0], 'col3': [0, 0, 0, 0], 'col4': [1, 1, 1, 1]}
+        X = pd.DataFrame(data=d)
         y = np.array([0, 0, 1, 1])
-        preds = np.array([0, 0, 0, 0])
+        preds = np.array([0, 0, 0, 1])
+        slice_column = 'col1'
 
         # Compute the model metrics
-        precision, recall, fbeta = compute_model_metrics(y, preds)
+        metrics_by_slice = compute_model_metrics_by_slice(X, y, preds, slice_column)
 
         # Check that the correct values are returned
-        self.assertEqual(precision, 0)
-        self.assertEqual(recall, 0)
-        self.assertEqual(fbeta, 0)
-
+        self.assertEqual(metrics_by_slice['A']['precision'], 1.0)
+        self.assertEqual(metrics_by_slice['A']['recall'], 1.0)
+        self.assertEqual(metrics_by_slice['A']['fbeta'], 1.0)
+        self.assertEqual(metrics_by_slice['B']['precision'], 1.0)
+        self.assertEqual(metrics_by_slice['B']['recall'], 0.5)
+        self.assertEqual(metrics_by_slice['B']['fbeta'], 0.6666666666666666)
 
 if __name__ == '__main__':
     unittest.main()
